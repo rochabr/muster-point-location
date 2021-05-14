b32dfaab7805c01a44b26d4a776c6289d4276768
Fernando Rocha <fernandorsl@gmail.com>
2021-05-14 00:01:53 -0700

    first commit

---
+++ muster-point-client/muster_point_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  muster_point_clientApp.swift
+//  muster-point-client
+//
+//  Created by Rocha Silva, Fernando on 2021-05-13.
+//
+
+import SwiftUI
+
+@main
+struct muster_point_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ muster-point-patrol/muster_point_patrolApp.swift
@@ -0,0 +1,17 @@
+//
+//  muster_point_patrolApp.swift
+//  muster-point-patrol
+//
+//  Created by Rocha Silva, Fernando on 2021-05-13.
+//
+
+import SwiftUI
+
+@main
+struct muster_point_patrolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
